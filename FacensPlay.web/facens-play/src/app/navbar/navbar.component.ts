eed7cb86b69e487a8c52cdb2865af6f631e0d6ff
Rafael Cortez <rafaelcortezp@gmail.com>
2024-06-26 14:15:55 -0300

    feat: implementação logout

--- FacensPlay.web/facens-play/src/app/navbar/navbar.component.ts
+++ FacensPlay.web/facens-play/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(
+    private router: Router,
+    private loginService: LoginService
+  ) { }
+
+  onLogout(): void {
+    this.loginService.logout()
+      .subscribe(() => {
+        localStorage.removeItem('access_token');
+        this.router.navigate(['/login']);
+    });
+  }
 
 }
